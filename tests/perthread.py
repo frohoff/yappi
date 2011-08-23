727672d484fd14ceafebeb31b36e173f5bcbb7b5
Sümer Cip <sumerc@gmail.com>
2011-08-23 21:11:18 +0300

    getrusage(RUSAGE_THREAD) is added and working. More testing is needed.

--- tests/perthread.py
+++ tests/perthread.py
@@ -5,17 +5,24 @@ import threading
 class MyThread(threading.Thread):
     def run(self):
         time.sleep(1)
-	# do a cpu-intensive work
-	i = 0
-	while(i < 1000000):
-	    i += 1
+        # do a cpu-intensive work
+        i = 0
+        j = 3
+        while(i < 100000):
+            i += 1
+            j *= i
 
-n = 1
-
-yappi.start()
-
-for i in range(0,n):
+def foo():
+    #n = 1
+    #for i in range(0,n):
     c = MyThread()
     c.start()
+    c.join()
+
+
+
+#yappi.start(builtins=True)
+yappi.start()
+foo()
 yappi.stop()
 yappi.print_stats()
